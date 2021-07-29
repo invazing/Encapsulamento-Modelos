9232f2379631ac68cb5d6452912f5157a8e49ce5
Invazing <invazing@hotmail.com>
2021-07-29 15:35:11 -0300

    Adicionar arquivos de projeto.

---
+++ CursoFoop_ModeloDominio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// modelo de encapsulamento rico e pobre
+// com passagem de parametros via construtor
+namespace CursoFoop_ModeloDominio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteRico cliente = new ClienteRico(-1,"", null);
+               
+            Console.WriteLine($"{cliente.Id} {cliente.Nome} {cliente.Endereco}");
+            Console.ReadLine();
+        }
+
+        private static ClienteAnemico ClienteAnemicoTeste()
+        {
+            ClienteAnemico cliente = new ClienteAnemico();
+            cliente.Id = -1;
+            cliente.Nome = "";
+            cliente.Endereco = null;
+            return cliente;
+        }
+    }
+}
